279deb3a7072b26d005bc9c1516b4e86bc208f3f
JohnGemini <john.chung@geminiopencloud.com>
2019-09-20 15:21:17 +0800

    refactor: modify the resource limits if not defined

--- main.go
+++ main.go
@@ -23,33 +23,33 @@ func main() {
     if err != nil {
         glog.Errorf("Filed to load key pair: %v", err)
     }
-	whsvr := &WebhookServer {
-		server:           &http.Server {
-			Addr:        ":443",
+    whsvr := &WebhookServer {
+        server:           &http.Server {
+            Addr:        ":443",
             TLSConfig:   &tls.Config{Certificates: []tls.Certificate{pair}},
-		},
+        },
         ResourceCPU:      string(corev1.ResourceCPU),
         ResourceMemory:   string(corev1.ResourceMemory),
         ResourceGPU:      ResourceGPU,
-	}
-	
-	// define http server and server handler
-	mux := http.NewServeMux()
-	mux.HandleFunc("/", whsvr.serve)
-	whsvr.server.Handler = mux
-	
-	// start webhook server in new rountine
-	go func() {
-		if err := whsvr.server.ListenAndServeTLS("", ""); err != nil {
-			glog.Errorf("Filed to listen and serve webhook server: %v", err)
-		}
-	}()
-	
-	// listening OS shutdown singal
-	signalChan := make(chan os.Signal, 1)
-	signal.Notify(signalChan, syscall.SIGINT, syscall.SIGTERM)
-	<-signalChan
-	
-	glog.Infof("Got OS shutdown signal, shutting down wenhook server gracefully...")
-	whsvr.server.Shutdown(context.Background())
+    }
+
+    // define http server and server handler
+    mux := http.NewServeMux()
+    mux.HandleFunc("/", whsvr.serve)
+    whsvr.server.Handler = mux
+
+    // start webhook server in new rountine
+    go func() {
+        if err := whsvr.server.ListenAndServeTLS("", ""); err != nil {
+            glog.Errorf("Filed to listen and serve webhook server: %v", err)
+        }
+    }()
+
+    // listening OS shutdown singal
+    signalChan := make(chan os.Signal, 1)
+    signal.Notify(signalChan, syscall.SIGINT, syscall.SIGTERM)
+    <-signalChan
+
+    glog.Infof("Got OS shutdown signal, shutting down wenhook server gracefully...")
+    whsvr.server.Shutdown(context.Background())
 }
--- webhook.go
+++ webhook.go
@@ -12,6 +12,7 @@ import (
     "k8s.io/api/admission/v1beta1"
     corev1 "k8s.io/api/core/v1"
     metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+    "k8s.io/apimachinery/pkg/api/resource"
     "k8s.io/client-go/kubernetes"
     "k8s.io/client-go/rest"
     "k8s.io/apimachinery/pkg/runtime"
@@ -36,22 +37,34 @@ var ignoredNamespaces = []string {
 }
 
 type WebhookServer struct {
-	server            *http.Server
+    server            *http.Server
     ResourceCPU       string
     ResourceGPU       string
     ResourceMemory    string
 }
 
+type patchOperation struct {
+    Op    string      `json:"op"`
+    Path  string      `json:"path"`
+    Value interface{} `json:"value,omitempty"`
+}
+
 // Check whether the target need to be validated
 func validationRequired(ignoredList []string, metadata *metav1.ObjectMeta) bool {
-	// skip special kubernete system namespaces
-	for _, namespace := range ignoredList {
-		if metadata.Namespace == namespace {
-			glog.Infof("Skip validation for %v for it's in special namespace: %v",
+    // skip the child object of the other resource
+    if ref := metadata.GetOwnerReferences(); len(ref) > 0 {
+        glog.Infof("Skip validation for %v for it's a child object of %v/%v",
+                   metadata.Name, ref[0].Kind, ref[0].Name)
+        return false
+    }
+    // skip special kubernete system namespaces
+    for _, namespace := range ignoredList {
+        if metadata.Namespace == namespace {
+            glog.Infof("Skip validation for %v for it's in special namespace: %v",
                        metadata.Name, metadata.Namespace)
-			return false
-		}
-	}
+            return false
+        }
+    }
 
     // get the in-cluster config
     config, err := rest.InClusterConfig()
@@ -60,20 +73,17 @@ func validationRequired(ignoredList []string, metadata *metav1.ObjectMeta) bool 
     }
     // get a k8s client
     client, err := kubernetes.NewForConfig(config)
-	if err != nil {
-		panic(err.Error())
-	}
+    if err != nil {
+        panic(err.Error())
+    }
     // get Namespace object by name
     namespace, err := client.CoreV1().Namespaces().Get(metadata.Namespace,
                                                        metav1.GetOptions{})
     if err != nil {
-		panic(err.Error())
-	}
+        panic(err.Error())
+    }
 
     annotations := namespace.ObjectMeta.GetAnnotations()
-    if annotations == nil {
-        annotations = map[string]string{}
-    }
     // determine whether to perform validation based on annotations of the target's namespace
     if val, ok := annotations[validateRequiredKey]; ok {
         if strings.Contains(val, validateRequiredValue) {
@@ -115,74 +125,137 @@ func getPodSpec(req *v1beta1.AdmissionRequest) (*corev1.PodSpec, error) {
     glog.Infof("AdmissionReview for Kind=%v Namespace=%v Name=%v",
                kind, namespace, name)
 
-	// determine whether to perform validation
-	if !validationRequired(ignoredNamespaces, metadata) {
+    // determine whether to perform validation
+    if !validationRequired(ignoredNamespaces, metadata) {
         return nil, nil
-	} else {
+    } else {
         return spec, nil
     }
 }
 
-func (whsvr *WebhookServer) limitsValidate(containers []corev1.Container) error {
+func (whsvr *WebhookServer) limitsValidate(container *corev1.Container) (bool, error) {
     resourceLimits := map[string]float64 {
         whsvr.ResourceCPU:    0,
         whsvr.ResourceGPU:    0,
         whsvr.ResourceMemory: 0,
     }
-    for _, container := range containers {
-        if container.Resources.Limits != nil {
-            limits := container.Resources.Limits
-            for resourceName := range resourceLimits {
-                if quantity, ok := limits[corev1.ResourceName(resourceName)]; ok {
-                    val := float64(quantity.Value())
-                    if resourceName == whsvr.ResourceMemory {
-                        val /= (1024 * 1024)
-                    }
-                    resourceLimits[resourceName] += val
-                } else {
-                    return errors.New(fmt.Sprintf("%v is required", resourceName))
+    patchRequired := false
+
+    if container.Resources.Limits != nil {
+        limits := container.Resources.Limits
+        for resourceName := range resourceLimits {
+            if quantity, ok := limits[corev1.ResourceName(resourceName)]; ok {
+                val := float64(quantity.Value())
+                if resourceName == whsvr.ResourceMemory {
+                    val /= (1024 * 1024)
                 }
+                resourceLimits[resourceName] += val
             }
-        } else {
-            return errors.New("container[*].resources.limits is required")
         }
+    } else {
+        return patchRequired, errors.New("container[*].resources.limits is required")
     }
-	glog.Infof("resourceLimits: %v", resourceLimits)
+
     limit_cpu := resourceLimits[whsvr.ResourceGPU] * 4
     limit_memory := resourceLimits[whsvr.ResourceGPU] * 90 * 1024
     if (resourceLimits[whsvr.ResourceGPU] <= 8 &&
         resourceLimits[whsvr.ResourceGPU] >= 1) {
         if resourceLimits[whsvr.ResourceCPU] > limit_cpu {
-            return errors.New(fmt.Sprintf("%v exceeds the limit of %v",
-                                          whsvr.ResourceCPU, limit_cpu))
-        } else if resourceLimits[whsvr.ResourceMemory] > limit_memory {
-            return errors.New(fmt.Sprintf("%v exceeds the limit of %v",
-                                          whsvr.ResourceMemory, limit_memory))
+            return patchRequired, errors.New(fmt.Sprintf(
+                "%v exceeds the limit of %v", whsvr.ResourceCPU, limit_cpu))
+        } else if resourceLimits[whsvr.ResourceCPU] == 0 {
+            resourceLimits[whsvr.ResourceCPU] = limit_cpu
+            quantity, err := resource.ParseQuantity(fmt.Sprintf("%v", limit_cpu))
+            if err != nil {
+                return patchRequired, errors.New(fmt.Sprintf(
+                    "Cannot parse value of %v: %v",
+                    whsvr.ResourceGPU, limit_cpu))
+            } else {
+                container.Resources.Limits[corev1.ResourceName(whsvr.ResourceCPU)] =
+                    quantity
+                patchRequired = true
+            }
+        }
+        if resourceLimits[whsvr.ResourceMemory] > limit_memory {
+            return patchRequired, errors.New(fmt.Sprintf(
+                "%v exceeds the limit of %v", whsvr.ResourceMemory, limit_memory))
+        } else if resourceLimits[whsvr.ResourceMemory] == 0 {
+            resourceLimits[whsvr.ResourceMemory] = limit_memory
+            quantity, err := resource.ParseQuantity(fmt.Sprintf("%vMi", limit_memory))
+            if err != nil {
+                return patchRequired, errors.New(fmt.Sprintf(
+                    "Cannot parse value of %v: %v",
+                    whsvr.ResourceMemory, limit_memory))
+            } else {
+                container.Resources.Limits[corev1.ResourceName(whsvr.ResourceMemory)] =
+                    quantity
+                patchRequired = true
+            }
         }
     } else {
-        return errors.New(fmt.Sprintf("%v exceeds the range from 1 to 8",
-                                      whsvr.ResourceGPU))
+        return patchRequired, errors.New(fmt.Sprintf(
+            "%v exceeds the range from 1 to 8", whsvr.ResourceGPU))
     }
-    return nil
+    glog.Infof("resourceLimits in container %v: %v",
+               container.Name, resourceLimits)
+    return patchRequired, nil
+}
+
+func (whsvr *WebhookServer) validateContainers(containers *[]corev1.Container, path string) ([]byte, error) {
+    var patch []patchOperation
+    patchRequired := false
+    for _, container := range *containers {
+        if pr, err := whsvr.limitsValidate(&container); err != nil {
+            return nil, err
+        } else {
+            patchRequired = patchRequired || pr
+        }
+    }
+    if patchRequired {
+        patch = append(patch, patchOperation {
+            Op:    "replace",
+            Path:  path,
+            Value: *containers,
+        })
+        return json.Marshal(patch)
+    } else {
+        return nil, nil
+    }
 }
 
 // main validation process
 func (whsvr *WebhookServer) validate(ar *v1beta1.AdmissionReview) *v1beta1.AdmissionResponse {
-	req := ar.Request
+    var path string
+    req := ar.Request
+    switch req.Kind.Kind {
+        case "ReplicationController":
+            path = "/spec/template/spec/containers"
+        case "Pod":
+            path = "/spec/containers"
+    }
     if spec, err := getPodSpec(req); err != nil {
         glog.Errorf("Could not unmarshal raw object: %v", err)
-		return &v1beta1.AdmissionResponse {
-			Result: &metav1.Status {
-				Message: err.Error(),
-			},
-		}
+        return &v1beta1.AdmissionResponse {
+            Result: &metav1.Status {
+                Message: err.Error(),
+            },
+        }
     } else if spec != nil {
-        if err := whsvr.limitsValidate(spec.Containers); err != nil {
+        if patchBytes, err := whsvr.validateContainers(&spec.Containers, path); err != nil {
             glog.Errorf("Validation fails: %v", err)
             return &v1beta1.AdmissionResponse {
                 Result: &metav1.Status {
                     Message: err.Error(),
                 },
+            }
+        } else if patchBytes != nil {
+            return &v1beta1.AdmissionResponse {
+                Allowed: true,
+                Patch:   patchBytes,
+                PatchType: func() *v1beta1.PatchType {
+                    pt := v1beta1.PatchTypeJSONPatch
+                    return &pt
+                }(),
             }
         }
     }
@@ -193,58 +266,58 @@ func (whsvr *WebhookServer) validate(ar *v1beta1.AdmissionReview) *v1beta1.Admis
 
 // Serve method for webhook server
 func (whsvr *WebhookServer) serve(w http.ResponseWriter, r *http.Request) {
-	var body []byte
-	if r.Body != nil {
-		if data, err := ioutil.ReadAll(r.Body); err == nil {
-			body = data
-		}
-	}
-	if len(body) == 0 {
-		glog.Error("empty body")
-		http.Error(w, "empty body", http.StatusBadRequest)
-		return
-	}
+    var body []byte
+    if r.Body != nil {
+        if data, err := ioutil.ReadAll(r.Body); err == nil {
+            body = data
+        }
+    }
+    if len(body) == 0 {
+        glog.Error("empty body")
+        http.Error(w, "empty body", http.StatusBadRequest)
+        return
+    }
 
-	// verify the content type is accurate
-	contentType := r.Header.Get("Content-Type")
-	if contentType != "application/json" {
-		glog.Errorf("Content-Type=%s, expect application/json", contentType)
-		http.Error(w, "invalid Content-Type, expect `application/json`",
+    // verify the content type is accurate
+    contentType := r.Header.Get("Content-Type")
+    if contentType != "application/json" {
+        glog.Errorf("Content-Type=%s, expect application/json", contentType)
+        http.Error(w, "invalid Content-Type, expect `application/json`",
                    http.StatusUnsupportedMediaType)
-		return
-	}
+        return
+    }
 
-	var admissionResponse *v1beta1.AdmissionResponse
-	ar := v1beta1.AdmissionReview{}
-	if _, _, err := deserializer.Decode(body, nil, &ar); err != nil {
-		glog.Errorf("Can't decode body: %v", err)
-		admissionResponse = &v1beta1.AdmissionResponse {
-			Result: &metav1.Status {
-				Message: err.Error(),
-			},
-		}
-	} else {
-		admissionResponse = whsvr.validate(&ar)
-	}
+    var admissionResponse *v1beta1.AdmissionResponse
+    ar := v1beta1.AdmissionReview{}
+    if _, _, err := deserializer.Decode(body, nil, &ar); err != nil {
+        glog.Errorf("Can't decode body: %v", err)
+        admissionResponse = &v1beta1.AdmissionResponse {
+            Result: &metav1.Status {
+                Message: err.Error(),
+            },
+        }
+    } else {
+        admissionResponse = whsvr.validate(&ar)
+    }
 
-	admissionReview := v1beta1.AdmissionReview{}
-	if admissionResponse != nil {
-		admissionReview.Response = admissionResponse
-		if ar.Request != nil {
-			admissionReview.Response.UID = ar.Request.UID
-		}
-	}
+    admissionReview := v1beta1.AdmissionReview{}
+    if admissionResponse != nil {
+        admissionReview.Response = admissionResponse
+        if ar.Request != nil {
+            admissionReview.Response.UID = ar.Request.UID
+        }
+    }
 
-	resp, err := json.Marshal(admissionReview)
-	if err != nil {
-		glog.Errorf("Can't encode response: %v", err)
-		http.Error(w, fmt.Sprintf("could not encode response: %v", err),
+    resp, err := json.Marshal(admissionReview)
+    if err != nil {
+        glog.Errorf("Can't encode response: %v", err)
+        http.Error(w, fmt.Sprintf("could not encode response: %v", err),
                    http.StatusInternalServerError)
-	}
-	glog.Infof("Ready to write reponse ...")
-	if _, err := w.Write(resp); err != nil {
-		glog.Errorf("Can't write response: %v", err)
-		http.Error(w, fmt.Sprintf("could not write response: %v", err),
+    }
+    glog.Infof("Ready to write reponse ...")
+    if _, err := w.Write(resp); err != nil {
+        glog.Errorf("Can't write response: %v", err)
+        http.Error(w, fmt.Sprintf("could not write response: %v", err),
                    http.StatusInternalServerError)
-	}
+    }
 }
